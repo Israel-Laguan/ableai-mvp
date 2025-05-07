13b17a32f25af425c23dead17cd5c520946f0719
Kembert Nieves <kembert@gmail.com>
2025-05-07 19:29:01 -0400

    Vercel test

--- apps/web-app/src/app/nx-welcome.tsx
+++ apps/web-app/src/app/nx-welcome.tsx
@@ -432,7 +432,7 @@ export function NxWelcome({ title }: { title: string }) {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome React {title} 👋
+              Welcome WEB APP {title} 👋
             </h1>
           </div>
 
