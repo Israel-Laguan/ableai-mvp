4503e22996e4d64c12a7c6295d4d1eb4cdd0bff1
Kembert <kembert@gmail.com>
2025-05-30 14:52:22 -0400

    feat(env): add Google Cloud credentials and redirect URL to environment configuration

--- apps/auth-api/src/config/env.config.ts
+++ apps/auth-api/src/config/env.config.ts
@@ -4,6 +4,21 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']),
   GIG_DB_URL: z.string(),
   PRIVATE_GIG_DB_URL: z.string(),
+  GOOGLE_APPLICATION_CREDENTIALS: z
+    .string()
+    .transform(v => JSON.parse(v))
+    .refine(v =>
+      z
+        .object({
+          projectId: z.string(),
+          clientEmail: z.string(),
+          privateKey: z.string().refine(val => val.includes('-----BEGIN PRIVATE KEY-----'), {
+            message: 'Invalid private key format',
+          }),
+        })
+        .parse(v)
+    ),
+  REDIRECT_AFTER_REGISTER_URL: z.string().url(),
 });
 
 export const env = envSchema.parse(process.env);
