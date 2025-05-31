e43d748f316d18526f4edbda12e94a876dc5f216
Kembert <kembert@gmail.com>
2025-05-31 10:54:37 -0400

    refactor(jwt): Add verifyToken interface

--- libs/product-domain/backend/src/shared/infrastructure/jwt/index.ts
+++ libs/product-domain/backend/src/shared/infrastructure/jwt/index.ts
@@ -1,18 +1,25 @@
+import { Services } from '@models/shared';
 import * as jwt from 'jsonwebtoken';
 
-export const makeJwtService = (secret: string) => ({
-  createToken: (payload: object) => {
-    const oneDay = '1 day';
-    return jwt.sign(payload, secret, {
-      expiresIn: oneDay,
-    });
-  },
-  verifyToken: (token: string) => {
+export type TokenVerificationOutput = string | jwt.JwtPayload;
+
+export const makeJwtService = (secret: string) => {
+  const verifyToken: Services.VerifyTokenService<TokenVerificationOutput> = (token: string) => {
     try {
       return jwt.verify(token, secret);
     } catch (error) {
       console.info((error as Error).message);
       throw error;
     }
-  },
-});
+  };
+
+  return {
+    createToken: (payload: object) => {
+      const oneDay = '1 day';
+      return jwt.sign(payload, secret, {
+        expiresIn: oneDay,
+      });
+    },
+    verifyToken,
+  };
+};
