e2731d06161f58bc7f47a2739f8cbbb7e124d03c
Kembert <kembert@gmail.com>
2025-06-02 14:37:39 -0400

    refactor(jwt): enhance createToken function to accept custom expiration time

--- libs/product-domain/backend/src/shared/infrastructure/jwt/index.ts
+++ libs/product-domain/backend/src/shared/infrastructure/jwt/index.ts
@@ -1,6 +1,8 @@
 import { Services } from '@models/shared';
 import * as jwt from 'jsonwebtoken';
 
+type ExpiresIn = jwt.SignOptions['expiresIn'];
+
 export type TokenVerificationOutput = string | jwt.JwtPayload;
 
 export const makeJwtService = (secret: string) => {
@@ -14,10 +16,9 @@ export const makeJwtService = (secret: string) => {
   };
 
   return {
-    createToken: (payload: object) => {
-      const oneDay = '1 day';
+    createToken: (payload: object, expiresIn: ExpiresIn = '1 day') => {
       return jwt.sign(payload, secret, {
-        expiresIn: oneDay,
+        expiresIn,
       });
     },
     verifyToken,
