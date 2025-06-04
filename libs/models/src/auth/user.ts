d17b1162a40a8a5d9934c9b5d448b3812f19c4d7
Kembert <kembert@gmail.com>
2025-06-04 10:25:29 -0400

    refactor(user): replace boolean with ENABLE_TYPE and string with LAST_APP_ROLE_TYPE for improved type safety

--- libs/models/src/auth/user.ts
+++ libs/models/src/auth/user.ts
@@ -6,11 +6,27 @@ export interface PrivateDataUser extends IBase {
   phoneNumber: string | null;
 }
 
+export enum LAST_APP_ROLE {
+  BUYER = 'BUYER',
+  WORKER = 'WORKER',
+}
+
+export enum ENABLE {
+  DISABLED_PERM = 'DISABLED_PERM',
+  ENABLE = 'ENABLE',
+  NOT_VERIFIED = 'NOT_VERIFIED',
+  TO_MANY_ATTEMPTS = 'TO_MANY_ATTEMPTS',
+}
+
+export type ENABLE_TYPE = (typeof ENABLE)[keyof typeof ENABLE];
+
+export type LAST_APP_ROLE_TYPE = (typeof LAST_APP_ROLE)[keyof typeof LAST_APP_ROLE];
+
 export interface User extends IBase {
   avatarUrl?: string | null;
-  enabled: boolean;
+  enabled: ENABLE_TYPE;
   displayName?: string | null;
-  lastAppRole?: string | null;
+  lastAppRole?: LAST_APP_ROLE_TYPE;
   lastViewBuyer?: string | null;
   lastViewWorker?: string | null;
   privateDataUserId: number;
