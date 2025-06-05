2badb88bdac319db5586c6e0a4a619d562e9dc3e
Kembert <kembert@gmail.com>
2025-06-05 09:02:31 -0400

    refactor(auth): remove unused gig and private-gig controllers and routes

--- apps/auth-api/src/controllers/index.ts
+++
@@ -1,2 +0,0 @@
-export * as GigController from './gig';
-export * as PrivateGigController from './private-gig';
--- apps/auth-api/src/routes/index.ts
+++ apps/auth-api/src/routes/index.ts
@@ -1,13 +1,9 @@
 import { Router } from 'express';
 
 import authRouter from '../modules/auth/routes';
-import gigRouter from './gig';
-import privateGigRouter from './private-gig';
 
 const apiV1 = Router();
 
-apiV1.use(gigRouter);
-apiV1.use(privateGigRouter);
 apiV1.use(authRouter);
 
 export default apiV1;
