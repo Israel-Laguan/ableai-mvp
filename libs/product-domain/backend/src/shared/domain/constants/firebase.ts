a925b07dcc91ac12a09f488622fbe4749b4c0bbb
Kembert <kembert@gmail.com>
2025-06-07 12:12:19 -0400

    fix(firebase): correct USER_NOT_FOUND error code to INVALID_CREDENTIALS for clarity

--- libs/product-domain/backend/src/shared/domain/constants/firebase.ts
+++ libs/product-domain/backend/src/shared/domain/constants/firebase.ts
@@ -1,7 +1,7 @@
 export enum FIREBASE_ERROR_CODES {
   EMAIL_ALREADY_EXISTS = 'auth/email-already-exists',
   INVALID_EMAIL = 'auth/invalid-email',
-  USER_NOT_FOUND = 'auth/user-not-found',
+  INVALID_CREDENTIALS = 'auth/user-not-found',
   UNAUTHORIZED_CONTINUE_URI = 'auth/unauthorized-continue-uri',
   UNKNOWN_ERROR = 'auth/unknown-error',
 }
