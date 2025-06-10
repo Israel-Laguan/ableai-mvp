d6eff404d567ddb473e17d8b4049945407594b83
Kembert <kembert@gmail.com>
2025-06-10 18:49:05 -0400

    refactor(auth): update Firebase register service to include fullName, password, and phoneNumber

--- libs/product-domain/backend/src/auth/infrastructure/firebase/services/register.ts
+++ libs/product-domain/backend/src/auth/infrastructure/firebase/services/register.ts
@@ -1,15 +1,15 @@
+import type { Infra } from '@models/auth';
 import type { FirebaseError, FirebaseUserRecord } from '../../../../shared/domain/interfaces';
 import type { FirebaseAuthModule } from '../../../../shared/domain/modules';
 
 import { FIREBASE_ERROR_CODES } from '../../../../shared/domain/constants';
-import { VerifyEmail } from '../../../domain/services';
-import { throwError } from '../errors';
+import { throwError, throwInvalidCredentialsError } from '../errors';
 
 const { INVALID_CREDENTIALS, UNKNOWN_ERROR } = FIREBASE_ERROR_CODES;
 const ERROR_PATH = 'AUTH_REGISTER_SERVICE';
 
-export function makeFirebaseRegisterService({ auth }: { auth: FirebaseAuthModule }): VerifyEmail {
-  return async ({ email }) => {
+export function makeFirebaseRegisterService({ auth }: { auth: FirebaseAuthModule }) {
+  return async ({ email, fullName, password, phoneNumber }: Infra.RegisterInput) => {
     const user: FirebaseUserRecord | null = await auth
       .getUserByEmail(email)
       .catch(async (error: unknown) => {
@@ -20,11 +20,15 @@ export function makeFirebaseRegisterService({ auth }: { auth: FirebaseAuthModule
         }
       });
 
-    if (!user) {
-      await auth.createUser({
-        email,
-        emailVerified: false,
-      });
+    if (user) {
+      throwInvalidCredentialsError(ERROR_PATH);
     }
+
+    return await auth.createUser({
+      displayName: fullName,
+      email,
+      password,
+      phoneNumber,
+    });
   };
 }
