75d464fdf83137c0a392ca376e82d726cc60b8ea
Kembert <kembert@gmail.com>
2025-06-10 20:05:12 -0400

    refactor(auth): remove unused validation schemas from AuthValidationInput

--- apps/auth-api/src/dependency-injection/auth-validation-input.ts
+++ apps/auth-api/src/dependency-injection/auth-validation-input.ts
@@ -1,26 +1,13 @@
 import { Express } from '@backend';
 import { Auth } from '@product-domain/backend';
 
-const { LoginUserSchema, EmailVerificationSchema, PhoneVerificationSchema, RegisterUserSchema } =
-  Auth.Infra.Zod.Schemas;
+const { RegisterUserSchema } = Auth.Infra.Zod.Schemas;
 
 const {
   Middlewares: { validateInputMiddleware },
 } = Express;
 
 export const AuthValidationInput = {
-  validateLoginUser: validateInputMiddleware({
-    bodySchema: LoginUserSchema,
-  }),
-
-  validatePhoneVerification: validateInputMiddleware({
-    bodySchema: PhoneVerificationSchema,
-  }),
-
-  validateQueryEmail: validateInputMiddleware({
-    querySchema: EmailVerificationSchema,
-  }),
-
   validateRegisterUser: validateInputMiddleware({
     bodySchema: RegisterUserSchema,
   }),
