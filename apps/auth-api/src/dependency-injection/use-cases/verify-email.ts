120ee7d9d7a14d7e55491ce137dd231c240467de
Kembert <kembert@gmail.com>
2025-06-10 20:02:22 -0400

    refactor(auth): remove email and phone verification use cases from dependency injection

--- apps/auth-api/src/dependency-injection/use-cases/verify-email.ts
+++
@@ -1,7 +0,0 @@
-import { Auth } from '@product-domain/backend';
-import { privateDataUserRepository, userRepository } from '../repositories';
-
-export const verifyEmail = Auth.App.MakeVerifyEmailUseCase({
-  userRepository,
-  privateDataUserRepository,
-});
