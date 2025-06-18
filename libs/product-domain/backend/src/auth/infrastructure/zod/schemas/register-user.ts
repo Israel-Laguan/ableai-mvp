bc6033c54598592af31135e782dd210a61b99264
Kembert <kembert@gmail.com>
2025-06-18 18:42:09 -0400

    refactor(auth): enforce strict validation in user registration and update schemas

--- libs/product-domain/backend/src/auth/infrastructure/zod/schemas/register-user.ts
+++ libs/product-domain/backend/src/auth/infrastructure/zod/schemas/register-user.ts
@@ -2,9 +2,11 @@ import { z } from 'zod';
 
 import { EmailSchema, FullNameSchema, PasswordSchema, PhoneNumberSchema } from './shared';
 
-export const RegisterUserSchema = z.object({
-  fullName: FullNameSchema,
-  email: EmailSchema,
-  phoneNumber: PhoneNumberSchema.optional(),
-  password: PasswordSchema,
-});
+export const RegisterUserSchema = z
+  .object({
+    fullName: FullNameSchema,
+    email: EmailSchema,
+    phoneNumber: PhoneNumberSchema.optional(),
+    password: PasswordSchema,
+  })
+  .strict();
