5e917ef33684381d25dfb906ed8e913ff6708a07
Kembert <kembert@gmail.com>
2025-06-18 16:43:04 -0400

    refactor(auth): refactor register and update use cases

--- libs/product-domain/backend/src/auth/application/update.ts
+++ libs/product-domain/backend/src/auth/application/update.ts
@@ -1,6 +1,3 @@
-import { zxcvbn, zxcvbnOptions } from '@zxcvbn-ts/core';
-import * as zxcvbnCommonPackage from '@zxcvbn-ts/language-common';
-
 import type { PrivateDataUser, User } from '@models/auth';
 import type { UpdateUserStatusKeys } from '../domain/constants';
 import type { MakeUpdateUserCaseConfig } from '../domain/interfaces';
@@ -20,28 +17,16 @@ interface RegisterErrorInputs {
 
 const {
   AUTH_DICTIONARY: { PRIVATE_USER_DATA_REPOSITORY, USER_REPOSITORY },
-  REGISTER_STATUS_CODE: { WEAK_PASSWORD },
-  AUTH_ERROR_MESSAGES: { WEAK_PASSWORD_MESSAGE, ERROR_MESSAGE, INVALID_CREDENTIALS_MESSAGE },
+  AUTH_ERROR_MESSAGES: { ERROR_MESSAGE, INVALID_CREDENTIALS_MESSAGE },
   UPDATE_STATUS_CODE: { INVALID_CREDENTIALS, ERROR_UPDATING_USER },
-  top10k10plus,
 } = Constants;
 
-const { throwError } = Errors.makeErrorRunner<
-  RegisterErrorInputs,
-  UpdateUserStatusKeys | typeof WEAK_PASSWORD
->({
+const { throwError } = Errors.makeErrorRunner<RegisterErrorInputs, UpdateUserStatusKeys>({
   [ERROR_UPDATING_USER]: () =>
     Errors.InternalServerError.create(ERROR_MESSAGE, 'UPDATE_USER_USE_CASE'),
 
   [INVALID_CREDENTIALS]: () =>
     Errors.UnauthorizeError.create(INVALID_CREDENTIALS_MESSAGE, 'AUTH_REGISTER'),
-
-  [WEAK_PASSWORD]: ({ feedback }) =>
-    Errors.BadRequestError.create(
-      `${WEAK_PASSWORD_MESSAGE}
-    ${feedback || 'No feedback provided'}.`,
-      'AUTH_REGISTER'
-    ),
 });
 
 const { makeBuildObjectDynamically } = Utils;
@@ -52,14 +37,6 @@ const buildUserUpdateObject = makeBuildObjectDynamically<User>({
 
 const buildPrivateDataUserUpdateObject = makeBuildObjectDynamically<PrivateDataUser>({
   admittedKeys: PRIVATE_DATA_USER_UPDATE_ADMITTED_KEYS,
-});
-
-zxcvbnOptions.setOptions({
-  dictionary: {
-    ...zxcvbnCommonPackage.dictionary,
-    'top-10k-10plus': top10k10plus,
-  },
-  graphs: zxcvbnCommonPackage.adjacencyGraphs,
 });
 
 export const makeUpdateUserUseCase = <
@@ -73,18 +50,6 @@ export const makeUpdateUserUseCase = <
   CustomOutput
 > => {
   return async input => {
-    const { password } = input;
-
-    if (password) {
-      const { score: passwordStrength, feedback } = zxcvbn(password);
-
-      const PASSWORD_STRENGTH_SCORE = 3;
-
-      if (passwordStrength < PASSWORD_STRENGTH_SCORE) {
-        throwError(WEAK_PASSWORD, { feedback: feedback.warning || 'No feedback provided' });
-      }
-    }
-
     const { rollback, ...result } = await runInUpdate(input);
 
     await runInTransaction(async repositoryManager => {
