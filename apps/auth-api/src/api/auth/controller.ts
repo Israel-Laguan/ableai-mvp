8a7d476f9190be42640842ec6b50d9c4ed531fce
Kembert <kembert@gmail.com>
2025-06-18 16:46:48 -0400

    refactor(auth): simplify login method and remove unused utility functions

--- apps/auth-api/src/api/auth/controller.ts
+++ apps/auth-api/src/api/auth/controller.ts
@@ -1,8 +1,7 @@
 import { Request, Response } from 'express';
 
 import { Express } from '@backend';
-import { CONSTANTS, Utils } from '@shared';
-import { Auth } from '@product-domain/backend';
+import { CONSTANTS } from '@shared';
 import { UpdateInput } from '../../interfaces';
 import { authService } from './service';
 
@@ -10,34 +9,21 @@ const { HTTP_STATUS_CODE } = CONSTANTS;
 
 const { tryCatchAndNext } = Express;
 
-const { makeBuildObjectDynamically } = Utils;
-
-const buildUpdateServiceInput = makeBuildObjectDynamically<
-  Auth.Domain.Interfaces.UpdateInput<UpdateInput>
->({
-  admittedKeys: ['idTokenClaims', 'user', 'lastPassword', 'password', 'privateDataUser'],
-});
-
-const buildUserUpdateServiceInput = makeBuildObjectDynamically({
-  admittedKeys: Auth.Domain.Constants.USER_UPDATE_ADMITTED_KEYS_WITH_ID,
-});
-
 export const authController = {
-  login: tryCatchAndNext(async (req: Request, res: Response) => {
-    const { email } = req.body;
-    const userAgent = req.headers['user-agent'];
-    const IP = req.headers['x-forwarded-for']?.[0] || req.socket.remoteAddress || req.ip;
-    const idToken = req.headers.authorization?.split('Bearer ')[1];
+  login: tryCatchAndNext(
+    async (req: Express.Types.AuthorizedRequest<UpdateInput['idTokenClaims']>, res: Response) => {
+      const userAgent = req.headers['user-agent'];
+      const IP = req.headers['x-forwarded-for']?.[0] || req.socket.remoteAddress || req.ip;
 
-    const { customToken, privateDataUser, user } = await authService.login({
-      email,
-      IP,
-      userAgent,
-      idToken,
-    });
+      const { privateDataUser, user } = await authService.login({
+        email: req.user.email,
+        IP,
+        userAgent,
+      });
 
-    res.status(HTTP_STATUS_CODE.OK).json({ customToken, privateDataUser, user });
-  }),
+      res.status(HTTP_STATUS_CODE.OK).json({ privateDataUser, user });
+    }
+  ),
 
   register: tryCatchAndNext(async (req: Request, res: Response) => {
     const result = await authService.register(req.body);
@@ -46,20 +32,14 @@ export const authController = {
 
   update: tryCatchAndNext(
     async (req: Express.Types.AuthorizedRequest<UpdateInput['idTokenClaims']>, res: Response) => {
-      const user = buildUserUpdateServiceInput({
-        ...req.body.user,
-        id: req.user.id,
-      });
-
-      const input = buildUpdateServiceInput({
+      const result = await authService.update({
         ...req.body,
         idTokenClaims: req.user,
-        user,
+        user: {
+          ...req.body.user,
+          id: req.user.id,
+        },
       });
-
-      const result = await authService.update(
-        input as Auth.Domain.Interfaces.UpdateInput<UpdateInput>
-      );
 
       res.status(HTTP_STATUS_CODE.OK).json(result);
     }
