6e0f95bffe5a5a55213a7a96ab8b48109ec388c0
Kembert <kembert@gmail.com>
2025-06-17 23:12:07 -0400

    feat(auth-api): add update input and output types along with update transaction interface

--- apps/auth-api/src/interfaces/index.ts
+++ apps/auth-api/src/interfaces/index.ts
@@ -1,4 +1,5 @@
 import { Infra } from '@models/auth';
+import { Transaction } from '@models/shared';
 import { Auth, Shared } from '@product-domain/backend';
 
 export type CustomLoginInput = Auth.Infra.Firebase.Types.FirebaseLoginInput;
@@ -10,3 +11,15 @@ export type LoginInput = Auth.Domain.Interfaces.LoginInput<CustomLoginInput>;
 export type RegisterInput = Infra.RegisterInput;
 
 export type RegisterOutput = Shared.Domain.Interfaces.FirebaseUserRecord;
+
+export type UpdateInput = Auth.Infra.Firebase.Types.FirebaseUpdateInput;
+
+export type UpdateOutput = Auth.Infra.Firebase.Types.FirebaseUpdateOutput;
+
+export type UpdateTransaction = Transaction.RunInTransaction<
+  {
+    PRIVATE_USER_DATA_REPOSITORY: Auth.Domain.Repositories.PrivateDataUserRepository;
+    USER_REPOSITORY: Auth.Domain.Repositories.UserRepository;
+  },
+  void
+>;
