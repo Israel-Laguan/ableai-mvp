16fac1434ed873bc8d4c5209d8bf2a0cab220038
Kembert <kembert@gmail.com>
2025-06-18 16:41:52 -0400

    refactor(auth): update register and update firebase services

--- libs/product-domain/backend/src/auth/infrastructure/firebase/services/register.ts
+++ libs/product-domain/backend/src/auth/infrastructure/firebase/services/register.ts
@@ -1,9 +1,9 @@
 import type { CreateRequest } from 'firebase-admin/lib/auth/auth-config';
 
-import type { Infra } from '@models/auth';
 import type { FirebaseError, FirebaseUserRecord } from '../../../../shared/domain/interfaces';
 import type { FirebaseAuthModule } from '../../../../shared/domain/modules';
 
+import { Utils } from '@shared';
 import { FIREBASE_ERROR_CODES } from '../../../../shared/domain/constants';
 import { AUTH_ERROR_MESSAGES } from '../../../domain/constants';
 import { RunInRegister } from '../../../domain/services';
@@ -13,13 +13,18 @@ const { INVALID_CREDENTIALS, EMAIL_ALREADY_EXISTS } = FIREBASE_ERROR_CODES;
 const { ALREADY_EXIST_MESSAGE, INVALID_PHONE_NUMBER_MESSAGE } = AUTH_ERROR_MESSAGES;
 const ERROR_PATH = 'AUTH_REGISTER_SERVICE';
 
+const buildFirebaseUserData = Utils.makeBuildObjectDynamically<CreateRequest>({
+  admittedKeys: ['displayName', 'email', 'emailVerified', 'password', 'phoneNumber'],
+  admittedFalsyKeys: ['emailVerified'],
+});
+
 export function makeFirebaseRegisterService({
   auth,
 }: {
   auth: FirebaseAuthModule;
 }): RunInRegister<Omit<FirebaseUserRecord, 'toJSON'>> {
-  return async ({ email, fullName, password, phoneNumber }: Infra.RegisterInput) => {
-    const user: FirebaseUserRecord | null = await auth
+  return async ({ password, privateDataUser: { email, fullName, phoneNumber }, user: { id } }) => {
+    const firebaseUser: FirebaseUserRecord | null = await auth
       .getUserByEmail(email)
       .catch(async (error: FirebaseError) => {
         if (error.code === INVALID_CREDENTIALS) {
@@ -29,28 +34,33 @@ export function makeFirebaseRegisterService({
         }
       });
 
-    if (user) {
+    if (firebaseUser) {
       throwError(EMAIL_ALREADY_EXISTS, ERROR_PATH, {
         message: ALREADY_EXIST_MESSAGE,
       });
     }
 
-    const userData: CreateRequest = {
+    const userData = buildFirebaseUserData({
       displayName: fullName,
       email,
-      emailVerified: false,
       password,
-    };
-
-    if (phoneNumber) {
-      userData.phoneNumber = phoneNumber;
-    }
+      phoneNumber,
+      emailVerified: false,
+    }) as CreateRequest;
 
     const newUser = (await auth.createUser(userData).catch((error: FirebaseError) => {
       throwError(error.code as FIREBASE_ERROR_CODES, ERROR_PATH, {
         message: INVALID_PHONE_NUMBER_MESSAGE,
       });
     })) as FirebaseUserRecord;
+
+    await auth
+      .setCustomUserClaims(newUser.uid, { id, roleId: 'user', lastAppRole: null })
+      .catch((error: FirebaseError) => {
+        throwError(error.code as FIREBASE_ERROR_CODES, ERROR_PATH, {
+          message: 'Failed to set custom user claims',
+        });
+      });
 
     return {
       ...newUser,
--- libs/product-domain/backend/src/auth/infrastructure/firebase/services/update.ts
+++ libs/product-domain/backend/src/auth/infrastructure/firebase/services/update.ts
@@ -1,91 +1,60 @@
 import type { UpdateRequest } from 'firebase-admin/lib/auth/auth-config';
-import type { UserRecord } from 'firebase-admin/lib/auth/user-record';
 
-import type { PrivateDataUser } from '@models/auth';
 import type { FirebaseError } from '../../../../shared/domain/interfaces';
 import type { FirebaseAuthModule } from '../../../../shared/domain/modules';
 import type { RunInUpdate } from '../../../domain/services';
-import type { FirebaseUpdateInput, FirebaseUpdateOutput } from '../types';
+import type { FirebaseUpdateInput } from '../types';
 
 import { FIREBASE_ERROR_CODES } from '../../../../shared/domain/constants';
 import { throwError, throwInvalidCredentialsError } from '../errors';
+import { Utils } from '@shared';
 
 const { INVALID_CREDENTIALS } = FIREBASE_ERROR_CODES;
 const ERROR_PATH = 'AUTH_REGISTER_SERVICE';
+
+const buildFirebaseUpdateRequest = Utils.makeBuildObjectDynamically<UpdateRequest>({
+  admittedKeys: ['displayName', 'email', 'phoneNumber'],
+});
 
 export function makeFirebaseUpdateService({
   auth,
 }: {
   auth: FirebaseAuthModule;
-}): RunInUpdate<FirebaseUpdateInput, FirebaseUpdateOutput> {
-  return async ({
-    idTokenClaims,
-    lastPassword,
-    password,
-    user: { displayName, id },
-    privateDataUser,
-  }) => {
-    const {
-      uid,
-      email: lastEmail,
-      phone_number: lastPhoneNumber,
-      email_verified,
-      roleId,
-      lastAppRole = null,
-    } = idTokenClaims;
+}): RunInUpdate<FirebaseUpdateInput> {
+  return async ({ idTokenClaims, user, privateDataUser }) => {
+    const { uid } = idTokenClaims;
 
-    if (!id || !roleId) {
+    const { id } = user;
+
+    if (!id) {
       throwInvalidCredentialsError(ERROR_PATH);
     }
 
-    const userUpdateData: UpdateRequest = {};
+    const { email, fullName, phoneNumber } = privateDataUser || {};
 
-    if (displayName) {
-      userUpdateData.displayName = displayName;
-    }
-    if (password && lastPassword) {
-      userUpdateData.password = password;
-    }
+    const updateRequestObj = buildFirebaseUpdateRequest({
+      displayName: fullName,
+      email,
+      phoneNumber,
+    });
 
-    const { email, phoneNumber } = privateDataUser || ({} as PrivateDataUser);
-
-    if (email) {
-      userUpdateData.email = email;
-    }
-    if (phoneNumber) {
-      userUpdateData.phoneNumber = phoneNumber;
-    }
-
-    const updatedUser = (await auth
-      .updateUser(uid, userUpdateData)
-      .catch(async (error: FirebaseError) => {
+    if (updateRequestObj) {
+      await auth.updateUser(uid, updateRequestObj).catch(async (error: FirebaseError) => {
         if (error.code === INVALID_CREDENTIALS) {
           return null;
         } else {
           return throwError(error.code as FIREBASE_ERROR_CODES, ERROR_PATH);
         }
-      })) as UserRecord;
-
-    const customToken = await auth.createCustomToken(uid, { id, roleId, lastAppRole }).catch(() => {
-      return throwInvalidCredentialsError(ERROR_PATH) as never;
-    });
+      });
+    }
 
     return {
-      ...updatedUser,
-      customToken,
       async rollback() {
-        await auth
-          .updateUser(uid, {
-            email: lastEmail,
-            emailVerified: email_verified,
-            password: lastPassword,
-            phoneNumber: lastPhoneNumber,
-          })
-          .catch((error: FirebaseError) => {
-            throwError(error.code as FIREBASE_ERROR_CODES, ERROR_PATH, {
-              message: 'Failed to rollback user update',
-            });
+        await auth.updateUser(uid, idTokenClaims).catch((error: FirebaseError) => {
+          throwError(error.code as FIREBASE_ERROR_CODES, ERROR_PATH, {
+            message: 'Failed to rollback user update',
           });
+        });
       },
     };
   };
