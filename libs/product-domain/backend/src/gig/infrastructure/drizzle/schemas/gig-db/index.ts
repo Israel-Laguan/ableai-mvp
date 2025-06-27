ac93b3b51e0a0d694835ff530ab6a5a499eceddb
Kembert <kembert@gmail.com>
2025-06-27 17:22:18 -0400

    feat(gig): add Review schema and update gig-db exports

--- libs/product-domain/backend/src/gig/infrastructure/drizzle/schemas/gig-db/index.ts
+++ libs/product-domain/backend/src/gig/infrastructure/drizzle/schemas/gig-db/index.ts
@@ -1,1 +1,7 @@
 export * from './gig-work';
+export * from './gig-work-team';
+export * from './recommendations';
+export * from './review';
+export * from './skill-hires';
+export * from './skills';
+export * from './statistics';
