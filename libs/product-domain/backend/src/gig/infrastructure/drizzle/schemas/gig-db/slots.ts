98afb7cea847cdf23e718400229cea0728663de6
Kembert <kembert@gmail.com>
2025-07-01 09:16:31 -0400

    fix(gig-db): rename table from "slot" to "slots" for consistency

--- libs/product-domain/backend/src/gig/infrastructure/drizzle/schemas/gig-db/slots.ts
+++ libs/product-domain/backend/src/gig/infrastructure/drizzle/schemas/gig-db/slots.ts
@@ -4,7 +4,7 @@ import type { Slot } from '@models/gig';
 
 import { Schemas } from '../../../../../shared/infrastructure/drizzle';
 
-export const slots = Schemas.withBaseSchema<Slot>('slot', {
+export const slots = Schemas.withBaseSchema<Slot>('slots', {
   workerId: p.integer('worker_id').notNull(),
   startTime: p.timestamp('start_time').notNull(),
   endTime: p.timestamp('end_time').notNull(),
--- libs/product-domain/backend/src/shared/infrastructure/drizzle/migrations/gig-db/0014_add-slot-availability.sql
+++ libs/product-domain/backend/src/shared/infrastructure/drizzle/migrations/gig-db/0014_add-slot-availability.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "slot" (
+CREATE TABLE "slots" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"worker_id" integer NOT NULL,
 	"start_time" timestamp NOT NULL,
@@ -8,7 +8,7 @@ CREATE TABLE "slot" (
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 
-ALTER TABLE "slot"
+ALTER TABLE "slots"
 ADD CONSTRAINT "slot_worker_id_fkey"
 FOREIGN KEY("worker_id") REFERENCES "workers"("id");
 
