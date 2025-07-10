b8c874b0c05e3c34374a4e7f0564023e17233805
Kembert <kembert@gmail.com>
2025-07-10 13:37:07 -0400

    fix(errors): correct error code for InternalServerError to match standard naming

--- libs/shared/src/errors/internal-server-error.ts
+++ libs/shared/src/errors/internal-server-error.ts
@@ -2,7 +2,7 @@ import { CustomError } from './custom-error';
 import { HTTP_STATUS_CODE } from '../constants';
 
 export class InternalServerError extends CustomError<typeof InternalServerError.CODE> {
-  static CODE = 'FORBIDDEN_ERROR';
+  static CODE = 'INTERNAL_SERVER_ERROR';
 
   private constructor(msg: string, path = 'Unknown path error') {
     super(InternalServerError.CODE, msg, HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR, path);
