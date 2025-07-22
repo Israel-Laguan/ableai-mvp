f3dac1c1e056bd1c9a178c2e574f52b37cdcdd95
Kembert <kembert@gmail.com>
2025-07-22 09:26:07 -0400

    fix(relations): correct variable name for privateDataUser relation

--- libs/product-domain/backend/src/shared/infrastructure/drizzle/schemas/private-gig-db/relations.ts
+++ libs/product-domain/backend/src/shared/infrastructure/drizzle/schemas/private-gig-db/relations.ts
@@ -3,7 +3,7 @@ import { relations } from 'drizzle-orm';
 import { paymentCards, privateDataUser } from '.';
 
 export const paymentCardsRelations = relations(paymentCards, ({ one }) => ({
-  privateDataUsers: one(privateDataUser, {
+  privateDataUser: one(privateDataUser, {
     fields: [paymentCards.privateDataUserId],
     references: [privateDataUser.id],
   }),
