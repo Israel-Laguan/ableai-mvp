4a1e4bdd9dcf0298af7841b93500493489f4ee41
Kembert <kembert@gmail.com>
2025-07-22 09:52:42 -0400

    fix(migrations): add foreign key constraint to payment_cards table

--- libs/product-domain/backend/src/shared/infrastructure/drizzle/migrations/private-gig-db/0010_fix-payment-cards.sql
+++ libs/product-domain/backend/src/shared/infrastructure/drizzle/migrations/private-gig-db/0010_fix-payment-cards.sql
@@ -1,2 +1,5 @@
-ALTER TABLE "payment_cards" ALTER COLUMN "card_number" SET DATA TYPE varchar(4);--> statement-breakpoint
-ALTER TABLE "payment_cards" ALTER COLUMN "provider" SET DATA TYPE varchar(255);
+ALTER TABLE payment_cards
+  ALTER COLUMN card_number SET DATA TYPE varchar(4),
+  ALTER COLUMN provider SET DATA TYPE varchar(255),
+  ADD CONSTRAINT payment_card_private_data_user_id_fkey
+    FOREIGN KEY (private_data_user_id) REFERENCES private_data_user(id) ON DELETE CASCADE;
