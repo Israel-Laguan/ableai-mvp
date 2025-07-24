ae759559294726beda8a624772f6125730488791
Kembert <kembert@gmail.com>
2025-07-24 09:00:51 -0400

    fix(gig): add error handling for worker registration

--- libs/product-domain/backend/src/gig/application/register-worker.ts
+++ libs/product-domain/backend/src/gig/application/register-worker.ts
@@ -1,5 +1,12 @@
 import { APP_ROLE } from '@models/shared';
 import { Constants, Interfaces, UseCases } from '../domain';
+import { Errors } from '@shared';
+
+type PgError = {
+  code: string;
+};
+
+const PATH = 'REGISTER_WORKER_USE_CASE';
 
 const {
   REGISTER_WORKER_REPOSITORIES: { STATISTIC_REPOSITORY, WORKER_REPOSITORY },
@@ -9,18 +16,26 @@ export function makeRegisterWorkerUseCase({
   runInTransaction,
 }: Interfaces.MakeRegisterWorkerInput): UseCases.RegisterWorker {
   return async input => {
-    return await runInTransaction(async repositoryManager => {
-      const workerRepository = repositoryManager.getRepository(WORKER_REPOSITORY);
-      const statisticRepository = repositoryManager.getRepository(STATISTIC_REPOSITORY);
-      const [[worker], statistic] = await Promise.all([
-        workerRepository.create(input),
-        statisticRepository.create({
-          appRole: APP_ROLE.WORKER,
-          userId: input.userId,
-        }),
-      ]);
+    try {
+      return await runInTransaction(async repositoryManager => {
+        const workerRepository = repositoryManager.getRepository(WORKER_REPOSITORY);
+        const statisticRepository = repositoryManager.getRepository(STATISTIC_REPOSITORY);
+        const [[worker], statistic] = await Promise.all([
+          workerRepository.create(input),
+          statisticRepository.create({
+            appRole: APP_ROLE.WORKER,
+            userId: input.userId,
+          }),
+        ]);
 
-      return { worker, statistic };
-    });
+        return { worker, statistic };
+      });
+    } catch (e) {
+      if ((e as PgError)?.code === '23505') {
+        throw Errors.AlreadyExistError.create('Worker already exists', PATH);
+      }
+
+      throw Errors.InternalServerError.create('Failed to register worker', PATH);
+    }
   };
 }
