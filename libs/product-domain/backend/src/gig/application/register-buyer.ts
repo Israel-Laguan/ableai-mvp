10ea63d85fe3a0295e53ccbb11250c6f29147f5e
Kembert <kembert@gmail.com>
2025-07-24 08:52:25 -0400

    fix(gig): implement error handling in register buyer use case

--- libs/product-domain/backend/src/gig/application/register-buyer.ts
+++ libs/product-domain/backend/src/gig/application/register-buyer.ts
@@ -1,5 +1,12 @@
 import { APP_ROLE } from '@models/shared';
 import { Constants, Interfaces, UseCases } from '../domain';
+import { Errors } from '@shared';
+
+type PgError = {
+  code: string;
+};
+
+const PATH = 'REGISTER_BUYER_USE_CASE';
 
 const {
   REGISTER_BUYER_REPOSITORIES: { BUYER_REPOSITORY, STATISTIC_REPOSITORY },
@@ -9,18 +16,26 @@ export function makeRegisterBuyerUseCase({
   runInTransaction,
 }: Interfaces.MakeRegisterBuyerInput): UseCases.RegisterBuyer {
   return async input => {
-    return await runInTransaction(async repositoryManager => {
-      const buyerRepository = repositoryManager.getRepository(BUYER_REPOSITORY);
-      const statisticRepository = repositoryManager.getRepository(STATISTIC_REPOSITORY);
-      const [[buyer], statistic] = await Promise.all([
-        buyerRepository.create(input),
-        statisticRepository.create({
-          appRole: APP_ROLE.BUYER,
-          userId: input.userId,
-        }),
-      ]);
+    try {
+      return await runInTransaction(async repositoryManager => {
+        const buyerRepository = repositoryManager.getRepository(BUYER_REPOSITORY);
+        const statisticRepository = repositoryManager.getRepository(STATISTIC_REPOSITORY);
+        const [[buyer], statistic] = await Promise.all([
+          buyerRepository.create(input),
+          statisticRepository.create({
+            appRole: APP_ROLE.BUYER,
+            userId: input.userId,
+          }),
+        ]);
 
-      return { buyer, statistic };
-    });
+        return { buyer, statistic };
+      });
+    } catch (e) {
+      if ((e as PgError)?.code === '23505') {
+        throw Errors.AlreadyExistError.create('Buyer already exists', PATH);
+      }
+
+      throw Errors.InternalServerError.create('Failed to register buyer', PATH);
+    }
   };
 }
