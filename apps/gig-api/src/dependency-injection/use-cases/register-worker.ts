3b1323dea303bac41ba2d1bf8d415b620d3df0e5
Kembert <kembert@gmail.com>
2025-07-28 13:31:17 -0400

    feat(gig-api): enhance worker registration with detailed skills, slots, and recommendations

--- apps/gig-api/src/dependency-injection/use-cases/register-worker.ts
+++ apps/gig-api/src/dependency-injection/use-cases/register-worker.ts
@@ -1,28 +1,6 @@
-import { Gig, Shared } from '@product-domain/backend';
-import { gigDb } from '../../db';
-
-const {
-  Domain: {
-    Constants: {
-      REGISTER_WORKER_REPOSITORIES: { WORKER_REPOSITORY },
-    },
-  },
-  Infra: {
-    Drizzle: {
-      Repositories: { makeDrizzleWorkerRepository },
-    },
-  },
-} = Gig;
-
-const runInTransaction: Gig.Domain.Services.RegisterWorkerTransaction =
-  Shared.Infra.Drizzle.Repositories.makeDrizzleUnitOfWork([
-    {
-      db: gigDb,
-      repositoryName: WORKER_REPOSITORY,
-      repositoryMaker: makeDrizzleWorkerRepository,
-    },
-  ]);
+import { Gig } from '@product-domain/backend';
+import { workerRepository } from '../repositories';
 
 export const registerWorker = Gig.App.makeRegisterWorkerUseCase({
-  runInTransaction,
+  workerRepository,
 });
--- libs/backend/src/express/types/index.ts
+++ libs/backend/src/express/types/index.ts
@@ -1,5 +1,15 @@
 import { Request } from 'express';
 
-export type AuthorizedRequest<UserCredentials extends object = object> = Request & {
+export type AuthorizedRequest<
+  UserCredentials extends object = object,
+  ReqParams extends Request['params'] = Request['params'],
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  ResBody extends object = any,
+  ReqBody extends object = any,
+  ReqQuery = qs.ParsedQs,
+  Locals extends Record<string, any> = Record<string, any>
+  /* eslint-enable @typescript-eslint/no-explicit-any */
+> = Request<ReqParams, ResBody, ReqBody, ReqQuery, Locals> & {
   user?: UserCredentials;
+  body?: ReqBody;
 };
