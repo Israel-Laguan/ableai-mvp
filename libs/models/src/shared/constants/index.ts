8247f5d757810e9ab484577b4c2d8ea404f85a3b
Kembert <kembert@gmail.com>
2025-07-31 12:31:03 -0400

    feat(product-domain/backend): add drizzle and zod utilities

--- libs/models/src/shared/constants/index.ts
+++ libs/models/src/shared/constants/index.ts
@@ -3,4 +3,8 @@ export enum APP_ROLE {
   WORKER = 'WORKER',
 }
 
+export const SORT = Object.freeze({ ASC: 'asc', DESC: 'desc' });
+
+export const SORTS = Object.values(SORT);
+
 export type APP_ROLE_TYPE = (typeof APP_ROLE)[keyof typeof APP_ROLE];
