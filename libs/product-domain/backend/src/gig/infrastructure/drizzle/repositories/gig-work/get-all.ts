c1cd95893cfb3c5483438a07157bcb80704cb832
Kembert <kembert@gmail.com>
2025-07-31 17:21:21 -0400

    fix(gig): fix total row count of getAll from "GigWorkRepository"

--- libs/product-domain/backend/src/gig/infrastructure/drizzle/repositories/gig-work/get-all.ts
+++ libs/product-domain/backend/src/gig/infrastructure/drizzle/repositories/gig-work/get-all.ts
@@ -13,25 +13,38 @@ type ValidGigWorksSortFields = (typeof Constants.VALID_GIG_WORK_SORT_FIELDS)[num
 
 export function makeGetAllGigWorks(db: NodePgDatabase): Repositories.GetAllGigWorks {
   return async ({ userId, limit = 10, offset = 0, sort }) => {
+    const whereClause = sql`
+      ${gigWorks.buyerId} = (
+        SELECT ${buyers.id} FROM ${buyers} WHERE ${buyers.userId} = ${userId}
+      )
+    `;
     const sortField = (sort?.split(':')[1] as ValidGigWorksSortFields) || 'createdAt';
     const sortBy = sql`${gigWorks[sortField]}`;
     const sortOrder = sort?.split(':')[0] === 'desc' ? 'DESC' : 'ASC';
 
-    const query = sql`
+    const gigWorksQuery = sql`
       SELECT *
       FROM ${gigWorks}
-      WHERE ${gigWorks.buyerId} = (
-        SELECT ${buyers.id} FROM ${buyers} WHERE ${buyers.userId} = ${userId}
-      )
+      WHERE ${whereClause}
       ORDER BY ${sortBy} ${sql.raw(sortOrder)}
-      LIMIT ${limit} OFFSET ${offset}
+      LIMIT ${limit}
+      OFFSET ${offset}
     `;
 
-    const queryResult = await db.execute(query);
+    const countQuery = sql`
+      SELECT COUNT(*)::int AS total
+      FROM ${gigWorks}
+      WHERE ${whereClause}
+    `;
+
+    const [gigWorksQueryResult, countQueryResult] = await Promise.all([
+      db.execute<Record<keyof GigWork, GigWork[keyof GigWork]>>(gigWorksQuery),
+      db.execute<{ total: number }>(countQuery),
+    ]);
 
     const currentPage = Math.floor(offset / limit) + 1;
-    const results = queryResult.rows as unknown as GigWork[];
-    const total = queryResult.rowCount || 0;
+    const results = gigWorksQueryResult.rows as GigWork[];
+    const total = countQueryResult.rows[0].total ?? 0;
     const totalPages = Math.ceil(total / limit);
 
     return {
