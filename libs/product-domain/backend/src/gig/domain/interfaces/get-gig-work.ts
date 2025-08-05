78fc4e20de9944efe0f820c35be3c470f4338b31
Kembert <kembert@gmail.com>
2025-08-05 15:57:30 -0400

    feat(gig): add GetOneGigWork interfaces

--- libs/product-domain/backend/src/gig/domain/interfaces/get-gig-work.ts
+++ libs/product-domain/backend/src/gig/domain/interfaces/get-gig-work.ts
@@ -6,10 +6,23 @@ export type MakeGetAllGigWorkInput = {
   gigWorkRepository: Repositories.GigWorkRepository;
 };
 
+export type MakeGetOneGigWorkInput = {
+  gigWorkRepository: Repositories.GigWorkRepository;
+  buyerRepository: Repositories.BuyerRepository;
+};
+
 export type GetAllGigWorkInput = Pick<GetAllInput, 'limit' | 'offset' | 'sort'> & {
+  userId: number;
+};
+
+export type GetOneGigWorkInput = Pick<GigWork, 'id'> & {
   userId: number;
 };
 
 export type GetAllGigWorkRequestQuery = Omit<GetAllGigWorkInput, 'userId'>;
 
+export type GetOneGigWorkRequestParams = { id: string };
+
 export type GetAllGigWorkOutput = PaginationResult<GigWork>;
+
+export type GetOneGigWorkOutput = GigWork;
