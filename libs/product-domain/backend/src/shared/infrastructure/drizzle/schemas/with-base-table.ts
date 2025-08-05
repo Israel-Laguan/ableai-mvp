8705a43f473517deed0d8d4507d7dffcf1698540
Kembert <kembert@gmail.com>
2025-08-05 15:53:29 -0400

    feat(drizzle): implement makeSelectSql function and update exports

--- libs/product-domain/backend/src/shared/infrastructure/drizzle/schemas/with-base-table.ts
+++ libs/product-domain/backend/src/shared/infrastructure/drizzle/schemas/with-base-table.ts
@@ -1,13 +1,15 @@
-import { IOmitBase } from '@models/shared';
-import { pgTable, serial, timestamp } from 'drizzle-orm/pg-core';
+import type { IOmitBase } from '@models/shared';
 import type { PgColumnBuilderBase } from 'drizzle-orm/pg-core';
+
+import { pgTable, serial, timestamp } from 'drizzle-orm/pg-core';
 
 export function withBaseSchema<Schema extends object>(
   tableName: string,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   columns: Record<keyof Omit<Schema, IOmitBase>, PgColumnBuilderBase<any, object>>
 ) {
-  return pgTable(tableName, {
+  const tableColumns = {
+    ...columns,
     id: serial('id').primaryKey(),
     ...columns,
     createdAt: timestamp('created_at').notNull().defaultNow(),
@@ -15,5 +17,44 @@ export function withBaseSchema<Schema extends object>(
       .notNull()
       .defaultNow()
       .$onUpdate(() => new Date()),
+  };
+
+  const pgTableInstance = pgTable(tableName, tableColumns);
+
+  type ColumnKey = keyof typeof tableColumns;
+
+  const columnKeys = Object.keys(tableColumns) as ColumnKey[];
+
+  type Column = {
+    [K in ColumnKey]: {
+      key: K;
+    };
+  };
+
+  const columnEntries = columnKeys.map((key: ColumnKey) => {
+    const value = {
+      key,
+    };
+    return [key, value] as const;
   });
+
+  const columnObjFromEntries = Object.fromEntries(columnEntries) as Column;
+
+  const column = Object.freeze(columnObjFromEntries);
+
+  Object.defineProperty(pgTableInstance, '_customs', {
+    value: {
+      data: {
+        columnKeys,
+        column,
+      },
+    },
+    writable: false,
+    enumerable: false,
+    configurable: false,
+  });
+
+  return pgTableInstance as typeof pgTableInstance & {
+    _customs: { data: { columnKeys: ColumnKey[]; column: Column } };
+  };
 }
--- libs/product-domain/backend/src/shared/infrastructure/drizzle/utils/index.ts
+++ libs/product-domain/backend/src/shared/infrastructure/drizzle/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './drizzle-express-crud-router';
 export * from './is-not-blank-sql';
+export * from './make-select-sql';
 export * from './make-set-sql';
 export * from './make-sql-array';
 export * from './make-where-sql';
