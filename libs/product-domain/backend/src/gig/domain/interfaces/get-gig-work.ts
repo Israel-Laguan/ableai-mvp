ce2ac50f7afb8a1750a41cbc205bbdbedf84407d
Kembert <kembert@gmail.com>
2025-08-11 17:31:59 -0400

    feat(gig): add getAllCompletedGigWorksResumes interfaces

--- libs/product-domain/backend/src/gig/domain/interfaces/get-gig-work.ts
+++ libs/product-domain/backend/src/gig/domain/interfaces/get-gig-work.ts
@@ -1,6 +1,6 @@
 import type { APP_ROLE, GetAllInput, PaginationResult } from '@models/shared';
 import type { Repositories } from '..';
-import type { GigWork, GigWorkStatus } from '@models/gig';
+import type { GigWork, GigWorkStatus, WorkerSkill } from '@models/gig';
 
 export type MakeGetAllGigWorkInput = {
   gigWorkRepository: Repositories.GigWorkRepository;
@@ -10,9 +10,17 @@ export type MakeGetOneGigWorkInput = {
   gigWorkRepository: Repositories.GigWorkRepository;
 };
 
+export type MakeGetAllCompletedGigWorkResumes = {
+  gigWorkRepository: Repositories.GigWorkRepository;
+};
+
 export type GetAllGigWorkInput = Pick<GetAllInput, 'limit' | 'offset' | 'sort'> & {
   status?: GigWorkStatus;
   appRole?: APP_ROLE;
+  userId: number;
+};
+
+export type GetAllCompletedGigWorkResumesInput = Pick<GetAllInput, 'limit' | 'offset' | 'sort'> & {
   userId: number;
 };
 
@@ -22,8 +30,20 @@ export type GetOneGigWorkInput = Pick<GigWork, 'id'> & {
 
 export type GetAllGigWorkRequestQuery = Omit<GetAllGigWorkInput, 'userId'>;
 
+export type GetAllCompletedGigWorkResumesRequestQuery = Omit<
+  GetAllCompletedGigWorkResumesInput,
+  'userId'
+>;
+
 export type GetOneGigWorkRequestParams = { id: string };
 
 export type GetAllGigWorkOutput = PaginationResult<GigWork>;
+
+export type CompletedGigWorkResumes = GigWork & {
+  totalPayment: number;
+  workers: (WorkerSkill & { uid: string })[];
+};
+
+export type GetAllCompletedGigWorkResumesOutput = PaginationResult<CompletedGigWorkResumes>;
 
 export type GetOneGigWorkOutput = GigWork;
