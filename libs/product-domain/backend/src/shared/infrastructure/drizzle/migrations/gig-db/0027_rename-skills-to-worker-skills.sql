ed34378bb5d2f4799380e47671ac239064a2cd06
Kembert Nieves <kembert@gmail.com>
2025-08-13 20:05:09 -0400

    fix migrations

--- libs/product-domain/backend/src/shared/infrastructure/drizzle/migrations/gig-db/0027_rename-skills-to-worker-skills.sql
+++ libs/product-domain/backend/src/shared/infrastructure/drizzle/migrations/gig-db/0027_rename-skills-to-worker-skills.sql
@@ -1,7 +1,6 @@
 ALTER TABLE "skills" RENAME TO "worker_skills";
 
-ALTER TABLE "gig_work_teams"
-    RENAME COLUMN "skill_id" TO "worker_skill_id",
-    ALTER COLUMN "total_payment" SET DEFAULT 0,
-    ALTER COLUMN "total_payment" DROP NOT NULL,
-    ALTER COLUMN "end_date_offer" SET NOT NULL;
+ALTER TABLE "gig_work_teams" RENAME COLUMN "skill_id" TO "worker_skill_id";
+ALTER TABLE "gig_work_teams" ALTER COLUMN "total_payment" SET DEFAULT 0;
+ALTER TABLE "gig_work_teams" ALTER COLUMN "total_payment" DROP NOT NULL;
+ALTER TABLE "gig_work_teams" ALTER COLUMN "end_date_offer" SET NOT NULL;
