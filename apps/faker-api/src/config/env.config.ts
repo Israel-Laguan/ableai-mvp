8059c665adb9bf63d46250a185e9167b0aeefe57
Kembert <kembert@gmail.com>
2025-08-14 11:33:43 -0400

    feat(faker-api): add endpoint to generate fake gig work payments with authorization middleware

--- apps/faker-api/src/config/env.config.ts
+++ apps/faker-api/src/config/env.config.ts
@@ -11,6 +11,24 @@ const envSchema = z.object({
 
   GIG_DB_URL: z.string(),
   PRIVATE_GIG_DB_URL: z.string(),
+
+  GOOGLE_SERVICE_ACCOUNT: z
+    .string()
+    .transform(v => {
+      const decoded = Buffer.from(v, 'base64').toString('utf8');
+      return JSON.parse(decoded);
+    })
+    .refine(v =>
+      z
+        .object({
+          project_id: z.string(),
+          client_email: z.string(),
+          private_key: z.string().refine(val => val.includes('-----BEGIN PRIVATE KEY-----'), {
+            message: 'Invalid private key format',
+          }),
+        })
+        .parse(v)
+    ),
 });
 
 const draw = envSchema.parse(process.env);
--- apps/faker-api/src/dependency-injection/index.ts
+++ apps/faker-api/src/dependency-injection/index.ts
@@ -1,1 +1,2 @@
 export * from './repositories';
+export * as Middlewares from './middlewares';
