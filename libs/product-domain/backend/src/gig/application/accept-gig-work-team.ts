569d810407bd4d31cfba94b95fc312d55564ba50
Kembert <kembert@gmail.com>
2025-08-20 13:18:10 -0400

    refactor(gig): simplify status assignment logic in makeAcceptGigWorkTeamUseCase

--- libs/product-domain/backend/src/gig/application/accept-gig-work-team.ts
+++ libs/product-domain/backend/src/gig/application/accept-gig-work-team.ts
@@ -1,6 +1,7 @@
 import { Errors } from '@shared';
 import { Interfaces, UseCases } from '../domain';
-import { Constants, GigWorkTeamStatus } from '@models/gig';
+import { Constants } from '@models/gig';
+import { match } from 'ts-pattern';
 
 const PATH = 'UPDATE_OFFER_STATUS';
 
@@ -8,11 +9,13 @@ export function makeAcceptGigWorkTeamUseCase({
   gigWorkTeamRepository,
 }: Interfaces.MakeAcceptGigWorkTeamInput): UseCases.AcceptGigWorkTeamUseCase {
   return async ({ accepted, ...input }) => {
-    let status: GigWorkTeamStatus = Constants.GIG_WORK_TEAM_STATUS.REJECTED;
-
-    if (accepted) {
-      status = Constants.GIG_WORK_TEAM_STATUS.ACCEPTED;
-    }
+    const status = match(accepted)
+      .with(true, () => {
+        return Constants.GIG_WORK_TEAM_STATUS.ACCEPTED;
+      })
+      .otherwise(() => {
+        return Constants.GIG_WORK_TEAM_STATUS.REJECTED;
+      });
 
     const gigWorkTeam = await gigWorkTeamRepository.updateStatus({
       ...input,
